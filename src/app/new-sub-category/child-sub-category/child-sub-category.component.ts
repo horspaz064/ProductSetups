bd4339a158b0d718c7ce9396c61bd4958f477b30
sokhenghun <Vindal554@gmail.com>
2024-11-02 10:11:40 +0700

    hello

---
+++ src/app/new-sub-category/child-sub-category/child-sub-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-child-new-sub",
+  templateUrl: "./child-sub-category.component.html",
+  styleUrl: ""
+})
+
+export class ChildNewSubCategory {
+  
+}
---
+++ src/app/new-sub-category/new-sub-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-new-sub",
+  templateUrl: "./new-sub-category.component.html",
+  styleUrl: ""
+})
+
+export class NewSubCategory {
+
+}
