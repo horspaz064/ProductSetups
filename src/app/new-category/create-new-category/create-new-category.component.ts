51da5b4a73c0b79d8bdf46949df98ad8cdad963a
PokPanha <pokpanha270@gmail.com>
2024-11-02 09:29:15 +0700

    commit

---
+++ src/app/new-category/create-new-category/create-new-category.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-create-new-category',
+  templateUrl:'create-new-category.component.html'
+})
+export class CreateNewCategoryComponent{
+
+}
---
+++ src/app/new-category/new-category.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+  selector:'app-new-category',
+  templateUrl:'./new-category.component.html'
+})
+export class NewCategoryComponent{
+
+}
