1bdd8b13d8520a32db2bf9967e62854dd19ad458
PokPanha <pokpanha270@gmail.com>
2024-11-02 10:36:23 +0700

    hello

--- src/app/new-category/create-new-category/create-new-category.component.ts
+++ src/app/new-category/create-new-category/create-new-category.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'app-create-new-category',
-  templateUrl:'create-new-category.component.html'
+  templateUrl:'./create-new-category.component.html',
+  styleUrl:'./create-new-category.component.css'
 })
 export class CreateNewCategoryComponent{
 
--- src/app/new-category/new-category.component.ts
+++ src/app/new-category/new-category.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 @Component({
   selector:'app-new-category',
-  templateUrl:'./new-category.component.html'
+  templateUrl:'./new-category.component.html',
+  styleUrl:'./new-category.component.css'
 })
 export class NewCategoryComponent{
 
