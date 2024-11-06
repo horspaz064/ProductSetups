481cafb78d08c6410fb767d9f11307222ea2ba2e
sokhenghun <Vindal554@gmail.com>
2024-11-06 14:39:35 +0700

    Hello world

---
+++ src/app/create-new-item/add-image-page/add-image-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-add-image",
+  templateUrl: "./add-image-page.component.html"
+})
+
+export class AddImagePageComponent {
+  
+}
---
+++ src/app/create-new-item/create-new-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "App-Create-New-Item",
+  templateUrl: "./create-new-item.component.html",
+})
+
+export class CreateNewItemComponent {
+  
+}
