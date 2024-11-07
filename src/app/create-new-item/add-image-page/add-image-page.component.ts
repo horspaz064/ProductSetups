f80113d29a1bfdea11f8178b225952e4cb8c1366
sokhenghun <Vindal554@gmail.com>
2024-11-07 15:59:54 +0700

    heng commit

--- src/app/create-new-item/add-image-page/add-image-page.component.ts
+++ src/app/create-new-item/add-image-page/add-image-page.component.ts
@@ -2,9 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-add-image",
-  templateUrl: "./add-image-page.component.html"
+  templateUrl: "./add-image-page.component.html",
+  styleUrl: "./add-imgage-page.component.css"
 })
 
 export class AddImagePageComponent {
-  
+
 }
